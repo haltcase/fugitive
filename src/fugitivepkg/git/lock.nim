10610d63bb542dab6e71a52de393dc49aeb672d8
citycide <thecitycide@gmail.com>
2017-10-22 17:15:33 -0500

    feat(lock): report failures vs sucesses separately

--- src/fugitivepkg/git/lock.nim
+++ src/fugitivepkg/git/lock.nim
@@ -26,7 +26,20 @@ proc lock* (args: Arguments, opts: Options) =
   if args.len < 1:
     fail "File name(s) must be provided."
 
-  let (res, code) = execCmdEx LOCK % [args.join " "]
-  if code != 0: fail res.strip
+  var good = 0
+  var untracked: seq[string] = @[]
+  for arg in args:
+    let (_, code) = execCmdEx cmdLockFile % arg
+    if code == 0: good.inc
+    else:
+      let (_, err) = execCmdEx "git ls-files --error-unmatch " & arg
+      if err == 1: untracked.add arg
 
-  print "File(s) locked."
+  if good > 0:
+    print "File(s) locked. (" & $good & " of " & $args.len & ")"
+    if untracked.len > 0:
+      echo "Could not lock untracked files:\n\n  " &
+        untracked.join "\n  "
+      echo ""
+  else:
+    fail "Untracked files cannot be locked."
